de25250626ba538971e02dcb379557f2c5542006
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-02-22 13:35:01 +0530

    Create sol.sql

---
+++ 1075. Project Employees I/sol.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select p.project_id , round(avg(e.experience_years),2)
+as average_years 
+from Employee e right join Project p 
+on p.employee_id = e.employee_id
+group by p.project_id;
