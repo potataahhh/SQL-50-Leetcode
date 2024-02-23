e94ed8ee3d45be2dc9fe38bbd0aec508c67c6a71
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-02-23 12:08:38 +0530

    Create sol.sql

---
+++ 1141. User Activity for the Past 30 Days I/sol.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select distinct activity_date as day , count(distinct user_id) as active_users 
+from Activity
+where activity_date <= '2019-07-27' and activity_date > '2019-06-27'
+group by activity_date;
