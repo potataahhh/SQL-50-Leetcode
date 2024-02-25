4ab509694507c6a9b31dcc9edce7ae3b0aa65cec
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-02-25 18:02:51 +0530

    Create sol.sql

---
+++ 1789. Primary Department for Each Employee/sol.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select employee_id , department_id
+from Employee
+group by employee_id 
+having count(employee_id)=1 UNION 
+select employee_id, department_id 
+from Employee
+where primary_flag ='Y';
