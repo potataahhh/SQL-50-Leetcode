09dfc1819ff66d4b43856c21e3e95a74e4cf93fe
Ayush Chandola <155428667+potataahhh@users.noreply.github.com>
2024-02-25 18:06:23 +0530

    Create sol.sql

---
+++ 1907. Count Salary Categories/sol.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select 'Low Salary' as category, count(account_id ) as accounts_count 
+from Accounts
+where income < 20000
+
+union
+
+select 'Average Salary' as category, count(account_id ) as accounts_count 
+from Accounts
+where income between 20000 and 50000
+
+union
+
+select 'High Salary' as category, count(account_id ) as accounts_count 
+from Accounts
+where income > 50000
